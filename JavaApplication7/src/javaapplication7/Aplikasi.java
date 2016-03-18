c7e1f4fa214858b0a5974371cd8c56ee75f67ddd
Dell Inspiron 14 <Dell Inspiron 14@10.190.12.10>
2016-03-18 15:15:14 +0700

    smua ok kecuali class aplikasi

---
+++ JavaApplication7/src/javaapplication7/Aplikasi.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication7;
+
+/**
+ *
+ * @author Dell Inspiron 14
+ */
+import java.util.Scanner;
+public class Aplikasi {
+	private Dokter[] daftarDokter;
+	private Pasien[] daftarPasien;
+	private Ruangan[] daftarRuangan;
+	final private int maxDokter = 100;
+	private int nDokter = 0;
+	final private int maxPasien = 1000;
+	private int nPasien = 0;
+	
+	public void addDokter(Dokter d){
+		if (nDokter < maxDokter){
+			daftarDokter[nDokter] = d;
+			nDokter++;
+		}
+		else{
+			System.out.println("Dokter sudah penuh");
+		}
+	}
+	
+	public void addPasien(Pasien p){
+		if (nPasien < maxPasien){
+			daftarPasien[nPasien] = p;
+			nPasien++;
+		}
+		else{
+			System.out.println("Pasien sudah penuh");
+		}
+	}
+	
+	public Pasien getPasien(int noReg){
+		return daftarPasien[noReg];
+ 	}
+	
+	public void deletePasien(Pasien p){
+		boolean found = false;
+		for(int i= 0; i<nPasien; i++){
+			if (daftarPasien[i].getNoRegistrasi() == p.getNoRegistrasi()){
+				found = true;
+				daftarPasien[i] = daftarPasien[nPasien-1];
+				nPasien--;
+				break;
+			}
+		}
+	}
+	
+	public void searchPasien(Pasien p){
+		int index = 0;
+		for (int i=0; i<nPasien; i++){
+			if(daftarPasien[i].getNoRegistrasi() == p.getNoRegistrasi());
+				index = i;
+		}
+	}
+	
+	public Dokter getDokter(int nip){
+		return daftarDokter[nip];
+	}
+	
+	public void deleteDokter(Dokter d){
+		boolean found = false;
+		for(int i= 0; i<nDokter; i++){
+			if (daftarDokter[i].getNip() == d.getNip()){
+				found = true;
+				daftarDokter[i] = daftarDokter[nDokter-1];
+				nDokter--;
+				break;
+			}
+		}
+	}
+	
+	public void searchDokter(Dokter d){
+		int index = 0;
+		for (int i=0; i<nPasien; i++){
+			if(daftarDokter[i].getNip() == d.getNip());
+				index = i;
+		}
+	}
+	public void mainMenu(){
+		System.out.println("Sistem Informasi Data Pasien Inap di Rumah Sakit");
+		System.out.println("================================================");
+		System.out.println("Daftar Menu");
+		System.out.println("1. Menambah Dokter ");
+		System.out.println("2. Menambah Pasien ");
+		System.out.println("3. Menghapus Dokter");
+		System.out.println("4. Menghapus Pasien");
+		System.out.println("5. Mencari Dokter");
+		System.out.println("6. Mencari Pasien");
+		System.out.println("\n");
+		System.out.println("Masukan Pilihan Anda : ");
+			int n = new Scanner(System.in).nextInt();
+		switch(n){
+			case 1:
+                            Dokter d = new Dokter();
+                            addDokter(d);
+				break;
+			case 2:
+                            Pasien p = new Pasien();
+				addPasien(p);
+				break;
+			case 3:
+				deleteDokter(d);
+				break;
+			case 4:
+				deletePasien(p);
+				break;
+			case 5:
+				searchDokter(d);
+				break;
+			case 6:
+				searchPasien(p);
+				break;
+			default:
+				System.out.println("Pilihan yang Dimasukkan Salah");
+		}
+	}
+} 
